bffabaca4af893664a91e482a04c029228de9014
Valera3107 <valerapetruna3107@gmail.com>
2019-06-05 15:18:19 +0300

    added new file for controller

--- photoGallery/src/main/java/com/ua/photoGallery/controller/UserController.java
+++ photoGallery/src/main/java/com/ua/photoGallery/controller/UserController.java
@@ -1,27 +1,17 @@
 package com.ua.photoGallery.controller;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ua.photoGallery.dao.ImageRepository;
@@ -81,26 +71,6 @@ public class UserController {
 		return "redirect:/login";
 	}
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public ModelAndView welcome() {
-		ModelAndView mav = new ModelAndView("mainPage");
-		mav.addObject("photos", imageRepository.findAll());
-		return mav;
-	}
-
-	@RequestMapping(value = "/uploadImage", method = RequestMethod.GET)
-	public String uploadImageGet() {
-		return "uploadImage";
-	}
-
-	@PostMapping(value = "/uploadImage")
-	public String uploadImage(@RequestParam MultipartFile image, @RequestParam String name,
-			@RequestParam String marker, @RequestParam String author) throws IOException {
-		imageService.storeFile(image, author, name, marker);
-
-		return "redirect:/home";
-	}
-
 	@RequestMapping(value = "/addToFavorite", method = RequestMethod.GET)
 	public ModelAndView addToFavorite(@RequestParam String id) {
 		User user = getUser.get();
@@ -135,14 +105,5 @@ public class UserController {
 		ModelAndView mav = new ModelAndView("favorite");
 		mav.addObject("favorites", user.getImages());
 		return mav;
-	}
-
-	@GetMapping(value = "/photo")
-	public ResponseEntity<Resource> downlaodFile(@RequestParam String id) throws FileNotFoundException {
-		Image fileMultipart = imageService.getFile(id);
-
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(fileMultipart.getFileType()))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileMultipart.getFileName() + "\"")
-				.body(new ByteArrayResource(fileMultipart.getData()));
 	}
 }
--- photoGallery/src/main/java/com/ua/photoGallery/domain/Image.java
+++ photoGallery/src/main/java/com/ua/photoGallery/domain/Image.java
@@ -3,9 +3,14 @@ package com.ua.photoGallery.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
-
-import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "image")
